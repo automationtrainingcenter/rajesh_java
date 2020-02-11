cb390e6fbe3d4a789099ac4e2c13c8c684b93309
automationtrainingcenter <atcsurya@gmail.com>
2020-02-11 10:32:43 +0530

    comments in java

---
+++ src/basics/CommentsDemo.java
@@ -0,0 +1,22 @@
+package basics;
+// comments are statements which will not execute
+// there are 3 types comments
+// 1. single line comment, 2. multi line comment and 3.documentation comment
+
+// 1. single line comment(//) is used to comment a single statement
+
+// 2. multiline comment (/*....*/) is used to comment multiple statements
+/*
+ * this is an example
+ * of the multiline 
+ * comment
+ */
+
+// 3. documentation comment (/**...*/) is used to create documentations for any class or method
+/**
+ * this is a class which explains
+ * how to write different comments in Java
+ */
+public class CommentsDemo {
+
+}
