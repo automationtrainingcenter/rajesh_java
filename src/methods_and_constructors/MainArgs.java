c9eb47d41fd0c55277b683ca3b99c3ac7b220322
automationtrainingcenter <atcsurya@gmail.com>
2020-03-12 10:55:26 +0530

    constructors

---
+++ src/methods_and_constructors/MainArgs.java
@@ -0,0 +1,11 @@
+package methods_and_constructors;
+
+public class MainArgs {
+	public static void main(String[] args) {
+		System.out.println(args.length);
+		for(String arg : args) {
+			System.out.println(arg);
+		}
+	}
+
+}
