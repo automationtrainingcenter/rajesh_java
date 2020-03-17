b1b81db9b4069d4e598a079ea26fbb3cd44cfcfc
automationtrainingcenter <atcsurya@gmail.com>
2020-03-17 10:34:34 +0530

    inheritance practicles

---
+++ src/inheritance/ChildOne.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class ChildOne extends Parent{
+	
+	public ChildOne() {
+//		super();
+		System.out.println("ChildOne class object created");
+	}
+
+	
+	public void methodTwo() {
+		System.out.println("method two of child one class");
+	}
+}
